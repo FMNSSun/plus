c5329fa027c3a37d432acc02fdd994d2145df719
Roman Muentener <munt@zhaw.ch>
2017-04-21 10:20:46 +0200

    * plus/conn: implement rudimentary sending/receiving

--- src/plus/conn/conn.go
+++ src/plus/conn/conn.go
@@ -4,14 +4,21 @@ import "net"
 import "time"
 import "plus/packet"
 import "fmt"
+import "log"
+import "os"
 
 type PLUSListener struct {
   packetConn net.PacketConn
-  plusConnections map[uint64]*PLUSConn
+  connections map[uint64]*PLUSConn
+  newConnections chan *PLUSConn
+  serverMode bool
+  logger *log.Logger
 }
+
 
 type PLUSConn struct {
   inChannel chan *packet.PLUSPacket
+  outChannel chan *packet.PLUSPacket
   packetConn net.PacketConn
   state uint16
   cat uint64
@@ -21,6 +28,7 @@ type PLUSConn struct {
   psn uint32
   kMaxOutOfOrder uint32
   remoteAddr net.Addr
+  logger *log.Logger
 }
 
 const maxPacketSize int = 4096
@@ -42,11 +50,45 @@ func ListenPLUS(laddr string) (*PLUSListener, error) {
   }
 
   var plusListener PLUSListener
+  plusListener.logger = log.New(os.Stdout, "Listener (true): ", log.Lshortfile)
   plusListener.packetConn = packetConn
+  plusListener.serverMode = true
+  plusListener.connections = make(map[uint64]*PLUSConn)
+  plusListener.newConnections = make(chan *PLUSConn)
 
   go plusListener.listen()
 
   return &plusListener, nil
+}
+
+func DialPLUS(laddr string, remoteAddr net.Addr) (*PLUSConn, error) {
+  packetConn, err := net.ListenPacket("udp", laddr)
+  
+  if(err != nil) {
+    return nil, err
+  }
+
+  var plusListener PLUSListener
+  plusListener.logger = log.New(os.Stdout, "Listener (false): ", log.Lshortfile)
+  plusListener.packetConn = packetConn
+  plusListener.serverMode = false
+  plusListener.connections = make(map[uint64]*PLUSConn)
+
+  go plusListener.listen()
+
+  // FIXME: Make this random. 
+  randomCAT := uint64(4) //totally random for now
+  plusListener.addConnection(randomCAT)
+
+  plusConnection, ok := plusListener.connections[randomCAT]
+
+  if(!ok) {
+    return nil, fmt.Errorf("Connection with CAT %d does not exist. BUG!", randomCAT)
+  }
+
+  plusConnection.updateRemoteAddr(remoteAddr)
+
+  return plusConnection, nil
 }
 
 func (conn *PLUSConn) RemoteAddr() net.Addr {
@@ -94,7 +136,10 @@ func (conn *PLUSConn) onStateStopRecv() {
 func (conn *PLUSConn) onStateClosed() {
 }
 
-func (conn *PLUSConn) sendPacket(plusPacket *packet.PLUSPacket) (int, error) {
+func (conn *PLUSConn) sendPacket(plusPacket *packet.PLUSPacket, size int) (int, error) {
+
+  conn.logger.Print(fmt.Sprintf("sendPacket: Sending packet PSN := %d, PSE := %d", plusPacket.PSN(), plusPacket.PSE()))
+  conn.logger.Print(plusPacket.Buffer())
   
   packetCAT := plusPacket.CAT()
 
@@ -123,13 +168,14 @@ func (conn *PLUSConn) sendPacket(plusPacket *packet.PLUSPacket) (int, error) {
     }
     break
   case STATE_CLOSED:
-    // Connection closed.
+    conn.logger.Print("sendPacket: Send on closed connection!")
     return 0,  fmt.Errorf("Connection has been closed.")
   }
 
   buffer := plusPacket.Buffer()
   buflen := len(buffer)
 
+  conn.logger.Print(fmt.Sprintf("sendPacket: WriteTo %s", conn.RemoteAddr().String()))
   n, err := conn.packetConn.WriteTo(buffer, conn.RemoteAddr())
 
   if(n != buflen) {
@@ -144,10 +190,17 @@ func (conn *PLUSConn) sendPacket(plusPacket *packet.PLUSPacket) (int, error) {
     return 0, err
   }
 
-  return 0, nil
+  return size, nil
 }
 
-func (conn *PLUSConn) onNewPacketReceived(plusPacket *packet.PLUSPacket)  {
+func (conn *PLUSConn) updateRemoteAddr(remoteAddr net.Addr) {
+  conn.remoteAddr = remoteAddr
+}
+
+func (conn *PLUSConn) onNewPacketReceived(plusPacket *packet.PLUSPacket, remoteAddr net.Addr)  {
+
+  conn.logger.Print(fmt.Sprintf("Received packet PSN := %d, PSE := %d", plusPacket.PSN(), plusPacket.PSE()))
+  conn.logger.Print(plusPacket.Buffer())
 
   packetCAT := plusPacket.CAT()
 
@@ -165,6 +218,8 @@ func (conn *PLUSConn) onNewPacketReceived(plusPacket *packet.PLUSPacket)  {
     // drop the packet
     return
   }
+
+  conn.updateRemoteAddr(remoteAddr)
 
   sendToChan := true
 
@@ -198,59 +253,100 @@ func (conn *PLUSConn) onNewPacketReceived(plusPacket *packet.PLUSPacket)  {
 }
 
 func (listener *PLUSListener) listen() {
+  listener.logger.Print(fmt.Sprintf("listen() on %s",listener.packetConn.LocalAddr().String()))
+
   buffer := make([]byte, maxPacketSize)
   for {
-    _, _, err := listener.packetConn.ReadFrom(buffer)
+    listener.logger.Print("listen: ReadFrom")
+    n, remoteAddr, err := listener.packetConn.ReadFrom(buffer)
+    listener.logger.Print("Read.")
 
     if(err != nil) {
       // FIXME: What the f*ck do we do here?
+      listener.logger.Printf("Reading from packetConn failed: %s", err.Error())
+      return
     } else {
-      plusPacket, err := packet.NewPLUSPacket(buffer)
+      plusPacket, err := packet.NewPLUSPacket(buffer[:n])
 
       if(err != nil) {
         // FIXME: What the f*ck do we do here?
+        listener.logger.Print("Parsing packet failed.")
       } else {
 
-        plusConnection, ok := listener.plusConnections[plusPacket.CAT()]
+        plusConnection, ok := listener.connections[plusPacket.CAT()]
 
         if(!ok) {
-          plusConnection = listener.addConnection(plusPacket.CAT())
+          if(listener.serverMode) {
+            plusConnection = listener.addConnection(plusPacket.CAT())
+          } else {
+            /* Bogus packet with a bogus cat. Skip */
+            listener.logger.Print("Bogus packet in non-servermode received")
+            continue
+          }
         }
 
-        plusConnection.onNewPacketReceived(plusPacket)
+        listener.logger.Print("Invoking onNewPacketReceived")
+
+        plusConnection.onNewPacketReceived(plusPacket, remoteAddr)
       }
     }
   }
 }
 
 func (listener *PLUSListener) addConnection(cat uint64) (*PLUSConn)  {
+  listener.logger.Print(fmt.Sprintf("addConnection: %d", cat))
+
   var plusConnection PLUSConn
-  plusConnection.state = STATE_UNIFLOW_RECV
+
+  /* Server mode or client mode? */
+  if(listener.serverMode) {
+    plusConnection.state = STATE_UNIFLOW_RECV
+    listener.logger.Print(fmt.Sprintf("addConnection: write to chan"))
+    listener.newConnections <- &plusConnection
+    listener.logger.Print(fmt.Sprintf("addConnection: wrote to chan"))
+  } else {
+    plusConnection.state = STATE_ZERO
+  }
+
   plusConnection.inChannel = make(chan *packet.PLUSPacket)
   plusConnection.cat = cat
-  listener.plusConnections[cat] = &plusConnection
+  listener.connections[cat] = &plusConnection
   plusConnection.defaultLFlag = false
   plusConnection.defaultRFlag = false
   plusConnection.packetConn = listener.packetConn
   plusConnection.pse = 0
   plusConnection.psn = 1
   plusConnection.kMaxOutOfOrder = uint32(mUint32/2)
+
+  plusConnection.logger = log.New(os.Stdout, fmt.Sprintf("Connection %d: ", cat), log.Lshortfile)
+
   return &plusConnection
 }
 
-func (*PLUSListener) Accept() (net.PacketConn, error) {
 
-  var plusConn PLUSConn
-
-  return &plusConn, nil
+func (listener *PLUSListener) Accept() (net.PacketConn, error) {
+  listener.logger.Print("Waiting for new connection...")
+  conn := <- listener.newConnections
+  listener.logger.Print("New connection \\o/")
+  return conn, nil
 }
 
-func (*PLUSConn) Close() error {
-  return nil
+func (listener *PLUSListener) Close() error {
+  // TODO: Close channels an all that stuff
+  listener.logger.Print("Close()")
+  return listener.packetConn.Close()
 }
 
-func (*PLUSConn) LocalAddr() net.Addr {
-  return nil
+func (conn *PLUSConn) Close() error {
+  // TODO: Maybe we need to do some cleanup?
+  // FIXME: This is obviously bullshit because all PLUSConn from the server share
+  //        the same packetConn. Maybe switch to channels?
+  conn.logger.Print("Close()")
+  return conn.packetConn.Close()
+}
+
+func (conn *PLUSConn) LocalAddr() net.Addr {
+  return conn.packetConn.LocalAddr()
 }
 
 func (conn *PLUSConn) ReadFrom(b []byte) (int, net.Addr, error) {
@@ -277,7 +373,7 @@ func (conn *PLUSConn) WriteTo(b []byte, addr net.Addr) (int, error) {
   plusPacket := packet.NewBasicPLUSPacket(conn.defaultLFlag, conn.defaultLFlag, false,
                    conn.cat, conn.psn, conn.pse, b)
 
-  return conn.sendPacket(plusPacket)
+  return conn.sendPacket(plusPacket, len(b))
 }
 
 func (*PLUSConn) SetDeadline(t time.Time) error {
