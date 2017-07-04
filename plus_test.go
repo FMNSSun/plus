6a74991b14f10ff64eaa4d66966e045d169f4da0
Roman Müntener <me@mroman.ch>
2017-07-04 12:22:38 +0200

    + more tests

--- plus_test.go
+++ plus_test.go
@@ -31,7 +31,7 @@ var _ = Describe("Plus", func() {
 		})
 
 		It("Deals with new extended packets", func() {
-			p, err := packet.NewExtendedPLUSPacket(false, false, false, 1234, 11, 12, 0x00, 0x00, []byte{0xCA,0xFE}, []byte{0xBA, 0xBE})
+			p, err := packet.NewExtendedPLUSPacket(false, false, false, 1234, 11, 12, 0x01, 0x00, []byte{0xCA,0xFE}, []byte{0xBA, 0xBE})
 
 			Expect(err).ToNot(HaveOccurred())
 			
@@ -50,6 +50,29 @@ var _ = Describe("Plus", func() {
 			Expect(conn.PSE()).To(Equal(uint32(11)))
 
 			Expect(feedbackData).To(Equal(p.Header()))
+		})
+
+		It("Deals with new extended packets with INTEGRITY_FULL", func() {
+			p, err := packet.NewExtendedPLUSPacket(false, false, false, 1234, 11, 12, 0x01, 0x03, []byte{0xCA,0xFE}, []byte{0xBA, 0xBE})
+
+			Expect(err).ToNot(HaveOccurred())
+			
+			packetConn := NewMockPacketConn()
+			packetConn.PutData(p.Buffer())
+			manager := NewConnectionManager(packetConn)
+			packet, addr, err := manager.ReadPacket()
+			Expect(err).ToNot(HaveOccurred())
+			Expect(packet).ToNot(Equal(nil))
+			Expect(addr).ToNot(Equal(nil))
+
+			conn, feedbackData, err := manager.ProcessPacket(p, addr)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(conn).ToNot(Equal(nil))
+			Expect(conn.CAT()).To(Equal(uint64(1234)))
+			Expect(conn.PSE()).To(Equal(uint32(11)))
+
+			var nilbuf []byte
+			Expect(feedbackData).To(Equal(nilbuf))
 		})
 	})
 })
